9f6a8a918034bea6541ffd8141d00e8ccd04d478
Bilal <you@example.com>
2020-11-01 18:59:45 +0100

    Crear el fichero index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juegos</title>
+</head>
+
+<body>
+    <h1>JUEGOS 2020</h1>
+    <?php
+    echo "<a href='juegaciam.php'>Juegaciam</a>";
+    ?>
+</body>
+
+</html>

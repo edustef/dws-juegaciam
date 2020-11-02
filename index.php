349278c3a9a69b2ddae533ec1a00df5e4bec3e2d
unknown <diegoclrx@gmail.com>
2020-11-03 00:04:36 +0100

    modificado el archivo index.php para darle otro diseño

--- index.php
+++ index.php
@@ -4,16 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/titulo.css">
     <title>Document</title>
 </head>
 
-<body>
-    <h1 class = "titulo" >JUEGOS 2020</h1>
+<body style="background-color: black;">
+    <header>
+        <h1 style="font-size: 50px;color:white; text-align: center; margin-top:5%">JUEGOS 2020</h1>
+        <div id="container" style="margin-left: 30%; margin-top: 10%;">
 
-    <?php
-    echo "<a href='juegaciam.php'>Juegaciam</a>";
-    ?>
+            <p><a href='juegaciam.php'>Juegaciam</a></p>
 
+        </div>
+
+    </header>
 </body>
 
 </html>
